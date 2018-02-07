2e4d731b0fefc4e54422faebeed89bf3af22f76d
Celina <celina.zaragoza@gmail.com>
2018-02-06 18:24:06 -0600

    fixed movies

--- liri.js
+++ liri.js
@@ -5,13 +5,31 @@ require("dotenv").config();
 
 //import the key.js file and store it in a variable
 var spotify = new Spotify(keys.spotify);
-var spotify = require("spotify");
-var client = Twitter(keys.twitter);
+var Spotify = require("node-spotify-api");
+var Twitter = Twitter("twitter");
+var request =require("request");
+var keys = require("./keys");
+var fs = require("fs");
+
 
 
 
 //* `my-tweets`: WILL SHOW THE LAST 20 TWEETS AND WHEN THEY WERE CREATED AT IN YOUR TERMINAL WINDOW.
-
+var getMyTweets = function() {
+  var client = new Twitter(keys.twitter);
+  var params = {
+    screen_name: "not_annAssassin"
+  };
+  client.get("statuses/user_timeline", params, function(error, tweets, response) {
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log("");
+        console.log(tweets[i].text);
+      }
+    }
+  });
+};
 
 //* `spotify-this-song`'<song name here'>: 
 		//SHOWS INFO IN TERMINAL:
@@ -53,5 +71,24 @@ function spotifyThisSong(songName) {
 		// SHOWS IN TERMINAL:
 		//TITLE OF MOVIE, YEAR, IMBD RATING, COUNTRY, LANGUAGE, PLOT,ACTORS, DEFAULT:MR.NOBODY
 		//USE 'trilogy' FOR OMDB API
-
+var getMeMovie = function(movieName) {
+  if (movieName === undefined) {
+    movieName = "Mr Nobody";
+  }
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var jsonData = JSON.parse(body);
+      console.log("Title: " + jsonData.Title);
+      console.log("Year: " + jsonData.Year);
+      console.log("Rated: " + jsonData.Rated);
+      console.log("IMDB Rating: " + jsonData.imdbRating);
+      console.log("Country: " + jsonData.Country);
+      console.log("Language: " + jsonData.Language);
+      console.log("Plot: " + jsonData.Plot);
+      console.log("Actors: " + jsonData.Actors);
+      console.log("Rotton Tomatoes URL: " + jsonData.tomatoURL);
+    }
+  });
+};
 //* `do-what-it-says`
